36d397fef24de4998e3e4fee68032652e662c07b
soorq <fantastfad830@gmail.com>
2024-10-08 17:42:52 +0300

    add: configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true, // Enable stricter type checking in development
+    swcMinify: true, // Enable swc minification in production builds
+    optimizeFonts: true, // Enable optimization for fonts
+    cleanDistDir: true,
+};
 
 export default nextConfig;
