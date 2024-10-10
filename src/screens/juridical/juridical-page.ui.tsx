474466fc9b28aed800d9909e7a570988f17d9407
Danil <fantastfad830@gmail.com>
2024-10-11 00:30:41 +0300

    fix:feat:review: layout/pages

--- src/screens/juridical/juridical-page.ui.tsx
+++ src/screens/juridical/juridical-page.ui.tsx
@@ -6,39 +6,61 @@ export function YuridiPage() {
     <>
       <Header />
       <main className="container flex min-h-[75dvh] gap-2.5">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et porro quo
-        ratione vel fugit excepturi, adipisci qui debitis! Eligendi saepe aut
-        vero temporibus. Reprehenderit quam ea magnam at alias deleniti. Atque
-        culpa in facilis, rerum officiis nostrum! Similique eligendi, voluptatem
-        reprehenderit iste, dolorum tempora obcaecati at numquam facilis
-        consectetur tenetur dignissimos velit voluptas nisi suscipit nobis? Et
-        deleniti reprehenderit quaerat! Laboriosam quos sint esse. Unde dolorem
-        dolor a, earum ipsam quasi officiis similique eveniet odio provident, ea
-        nam temporibus deserunt accusantium perspiciatis. Exercitationem
-        consectetur, ratione officiis facere sunt tempore nisi! Dicta, rem illo
-        temporibus, iusto, expedita repellat dolores ex aperiam voluptates
-        facilis non quae nemo adipisci iste? Deleniti nihil vero nemo iure
-        totam. Iste magni nostrum voluptatem, fuga laboriosam blanditiis? Neque
-        eius facere, laudantium aut, quis debitis, error minima tempore adipisci
-        omnis dolorem voluptatibus quos! Totam, voluptate consequuntur accusamus
-        reiciendis temporibus molestiae exercitationem est accusantium.
-        Aspernatur iusto omnis neque fuga? Quasi, animi ipsum, consectetur,
-        rerum eligendi laudantium consequatur error dolore quia saepe nisi culpa
-        maxime omnis? Incidunt blanditiis praesentium voluptatem animi ducimus,
-        sequi iusto aperiam adipisci in, itaque quasi explicabo. Eveniet nam vel
-        voluptas eaque natus suscipit dolore molestiae pariatur. Numquam quam
-        quas nam animi eveniet laudantium quos distinctio veritatis dolorum
-        repellat, illum nihil ex, eos saepe sunt ut neque! Dolor nesciunt
-        laudantium nulla ab asperiores, sapiente cum soluta esse temporibus vero
-        sed nostrum perferendis eaque eligendi non amet mollitia a, eos maxime
-        minus voluptates alias, repudiandae tenetur. Repellat, ipsum. Eius,
-        ullam vel. Dolores omnis perferendis exercitationem quos optio nobis
-        tempore. Inventore, sit deserunt numquam saepe ea fugit fuga sint cum
-        totam labore eius modi animi, voluptatem incidunt vel exercitationem?
-        Dicta nemo quidem pariatur explicabo nihil culpa nisi incidunt maxime,
-        sequi in repellendus ea odio magnam non. Obcaecati neque, vero nemo modi
-        culpa cupiditate, velit voluptatibus reprehenderit veritatis, quis
-        nihil?
+        <div className="w-1/3"></div>{" "}
+        <div className="lg:w-2/3 lg:border-l lg:pl-5">
+          Юридическая Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Et porro quo ratione vel fugit excepturi, adipisci qui debitis!
+          Eligendi saepe aut vero temporibus. Reprehenderit quam ea magnam at
+          alias deleniti. Atque culpa in facilis, rerum officiis nostrum!
+          Similique eligendi, voluptatem reprehenderit iste, dolorum tempora
+          obcaecati at numquam facilis consectetur tenetur dignissimos velit
+          voluptas nisi suscipit nobis? Et deleniti reprehenderit quaerat!
+          Laboriosam quos sint esse. Unde dolorem dolor a, earum ipsam quasi
+          officiis similique eveniet odio provident, ea nam temporibus deserunt
+          accusantium perspiciatis. Exercitationem consectetur, ratione officiis
+          facere sunt tempore nisi! Dicta, rem illo temporibus, iusto, expedita
+          repellat dolores ex aperiam voluptates facilis non quae nemo adipisci
+          iste? Deleniti nihil vero nemo iure totam. Iste magni nostrum
+          voluptatem, fuga laboriosam blanditiis? Neque eius facere, laudantium
+          aut, quis debitis, error minima tempore adipisci omnis dolorem
+          voluptatibus quos! Totam, voluptate consequuntur accusamus reiciendis
+          temporibus molestiae exercitationem est accusantium. Aspernatur iusto
+          omnis neque fuga? Quasi, animi ipsum, consectetur, rerum eligendi
+          laudantium consequatur error dolore quia saepe nisi culpa maxime
+          omnis? Incidunt blanditiis praesentium voluptatem animi ducimus, sequi
+          iusto aperiam adipisci in, itaque quasi explicabo. Eveniet nam vel
+          voluptas eaque natus suscipit dolore molestiae pariatur. Numquam quam
+          quas nam animi eveniet laudantium quos distinctio veritatis dolorum
+          repellat, illum nihil ex, eos saepe sunt ut neque! Dolor nesciunt
+          laudantium nulla ab asperiores, sapiente cum soluta esse temporibus
+          vero sed nostrum perferendis eaque eligendi non amet mollitia a, eos
+          maxime minus voluptates alias, repudiandae tenetur. Repellat, ipsum.
+          Eius, ullam vel. Dolores omnis perferendis exercitationem quos optio
+          nobis tempore. Inventore, sit deserunt numquam saepe ea fugit fuga
+          sint cum totam labore eius modi animi, voluptatem incidunt vel
+          exercitationem? Dicta nemo quidem pariatur explicabo nihil culpa nisi
+          incidunt maxime, sequi in repellendus ea odio magnam non. Obcaecati
+          neque, vero nemo modi culpa cupiditate, velit voluptatibus
+          reprehenderit veritatis, quis nihil? Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Voluptate molestiae, optio dignissimos
+          soluta nobis voluptas magnam, velit, mollitia nostrum similique
+          molestias obcaecati quia blanditiis. Repellendus, ratione! Ab
+          reprehenderit autem deleniti. Facere a sequi nobis eveniet, corporis,
+          delectus nisi magni architecto cupiditate debitis soluta aliquid
+          fugiat neque asperiores facilis et non reprehenderit dignissimos
+          deleniti! Veritatis aspernatur voluptas dolore, excepturi harum
+          temporibus. Pariatur tenetur sed delectus fugit modi similique
+          quisquam animi voluptatum, nisi officiis. Officiis sapiente quod,
+          natus dolore reiciendis, voluptatibus distinctio ex perferendis
+          excepturi praesentium, assumenda quibusdam. Et natus ratione illo?
+          Facilis, quo nulla ab in iusto ratione illum magnam consequatur
+          adipisci modi incidunt, magni fugit aut recusandae dolor nostrum
+          voluptatum blanditiis neque vitae beatae? Fugiat tenetur aspernatur
+          architecto dolorum dignissimos. Architecto libero adipisci quam eos
+          tempora facere, consectetur cupiditate esse, quaerat assumenda vitae
+          nobis corrupti soluta delectus porro. Facilis molestiae fuga error
+          tenetur dolor animi repudiandae soluta porro repellendus quo!
+        </div>
       </main>
       <Footer />
     </>
